3f80a58c0c7cca72f6eed4b02876e1791a590a3a
Parapass <59424861+Parapass@users.noreply.github.com>
2020-07-02 07:38:52 -0400

    Update core.js

--- core.js
+++ core.js
@@ -20,7 +20,7 @@ client.on('message', msg=>{
 })
 client.on('message', msg=>{
     if (msg.content === `-ping`) {
-        const msg = await message.channel.send('Pinging Heroku servers..')
+        const msg = message.channel.send('Pinging Heroku servers..')
         const ping = msg.createdTimestamp - message.createdTimestamp;
         msg.edit(`Ping: ${ping}`)
     }
