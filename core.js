5ff93ff8ae55dfe9ea8735a2bf8203713e9863a6
Parapass <59424861+Parapass@users.noreply.github.com>
2020-07-02 07:35:40 -0400

    Update core.js

--- core.js
+++ core.js
@@ -20,8 +20,15 @@ client.on('message', msg=>{
 })
 client.on('message', msg=>{
     if (msg.content === `-ping`) {
+        const msg = await message.channel.send('Pinging Heroku servers..')
         const ping = msg.createdTimestamp - message.createdTimestamp;
         msg.edit(`Ping: ${ping}`)
+    }
+    client.off
+})
+client.on('message', msg=>{
+    if (msg.content === `-uptime`) {
+        msg.channel.send(`Bot uptime is currently \'${ms(this.client.uptime, { long: true }}}\'`)
     }
     client.off
 })
