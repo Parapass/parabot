cf19c3c24673ae78fa4c5f476eefa1538a76af43
Parapass <59424861+Parapass@users.noreply.github.com>
2020-07-03 08:32:39 -0400

    Update core.js

--- core.js
+++ core.js
@@ -45,7 +45,7 @@ client.on('message', msg=>{
     if (msg.content === `-roleadd youtube`) {
         let role = message.guild.roles.find(r => r.name === "Youtube Pings");
 // Let's pretend you mentioned the user you want to add a role to (!addrole @user Role Name):
-let member = message.mentions.members.first();
+let member = message.member;
 // or the person who made the command: let member = message.member;
 // Add the role!
 member.addRole(role).catch(console.error);
