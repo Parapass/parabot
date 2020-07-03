6c87494a95b3146f5d3d93996b1efe125682ea31
Parapass <59424861+Parapass@users.noreply.github.com>
2020-07-03 14:10:57 -0400

    Update core.js

--- core.js
+++ core.js
@@ -30,6 +30,14 @@ client.on('message', msg=>{
     client.off
 })
 client.on('message', msg=>{
+    if (msg.content === `-uptime`) {
+        module.exports = class extends Command {
+          async run(message) {
+            msg.channel.send('My uptime is \'${ms(this.client.uptime, { long: true }}}\');
+    }
+    client.off
+})
+client.on('message', msg=>{
     if (msg.content === `-roleadd twitch`) {
         // Set the member's roles to a single role
        var role= msg.member.guild.roles.cache.find(role => role.name === "Twitch Pings");
