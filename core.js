052beda079b661d8d7cbfad06cb0817a58dafe64
Parapass <59424861+Parapass@users.noreply.github.com>
2020-07-03 09:14:08 -0400

    Update core.js

--- core.js
+++ core.js
@@ -31,9 +31,9 @@ client.on('message', msg=>{
 })
 client.on('message', msg=>{
     if (msg.content === `-roleadd twitch`) {
-        let role = client.guild.roles.find(r => r.name === "Twitch Pings");
+        const role = client.guild.roles.find(r => r.name === "Twitch Pings");
 // Let's pretend you mentioned the user you want to add a role to (!addrole @user Role Name):
-let member = client.member;
+const member = client.member;
 // or the person who made the command: let member = message.member;
 // Add the role!
 member.addRole(role).catch(console.error);
