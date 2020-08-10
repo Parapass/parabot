f456adac5626902eefca2e430a3edcdbc4b78854
Parapass <59424861+Parapass@users.noreply.github.com>
2020-08-10 09:37:14 -0400

    fex

--- roles.js
+++ roles.js
@@ -1,6 +1,9 @@
 const { Client, MessageEmbed } = require('discord.js');
 const Discord = require('discord.js');
 const client = new Discord.Client();
+
+client.login(process.env.BOT_TOKEN)
+
 client.on("ready", () => {
   console.log('Roles init')
 })
