7226d57ca925f22a2ef4b015f7c6b933e4d3c4f3
Parapass <59424861+Parapass@users.noreply.github.com>
2020-08-10 09:34:17 -0400

    Update roles.js

--- roles.js
+++ roles.js
@@ -5,9 +5,9 @@ client.on('message', msg=>{
     if (msg.content === `-roleadd twitch`) {
         // Set the member's roles to a single role
        var role= msg.member.guild.roles.cache.find(role => role.name === "Twitch Pings");
-       msg.member.roles.add(role);
-      console.log('Added twitch role successfully.')
-      msg.channel.send('Added Twitch Pings role successfully.')
+       msg.member.roles.remove(role);
+      console.log('Removed twitch role successfully.')
+      msg.channel.send('Removed Twitch Pings role successfully.')
     }
   client.off
 })
@@ -15,9 +15,9 @@ client.on('message', msg=>{
     if (msg.content === `-roleadd youtube`) {
         // Set the member's roles to a single role
        var role= msg.member.guild.roles.cache.find(role => role.name === "Youtube Pings");
-       msg.member.roles.add(role);
-      console.log('Added youtube role successfully.')
-      msg.channel.send('Added Youtube Pings role successfully.')
+       msg.member.roles.remove(role);
+      console.log('Removed youtube role successfully.')
+      msg.channel.send('Removed Youtube Pings role successfully.')
     }
   client.off
 })
@@ -25,9 +25,9 @@ client.on('message', msg=>{
     if (msg.content === `-roleadd tiktok`) {
         // Set the member's roles to a single role
        var role= msg.member.guild.roles.cache.find(role => role.name === "TikTok Pings");
-       msg.member.roles.add(role);
-      console.log('Added TikTok role successfully.')
-      msg.channel.send('Added TikTok Pings role successfully.')
+       msg.member.roles.remove(role);
+      console.log('Removed TikTok role successfully.')
+      msg.channel.send('Removed TikTok Pings role successfully.')
     }
   client.off
 })
