8d42ddac80d7e954b7edae94816c823a3ef481d0
Parapass <59424861+Parapass@users.noreply.github.com>
2020-08-10 09:35:32 -0400

    Update roles.js

--- roles.js
+++ roles.js
@@ -2,7 +2,7 @@ const { Client, MessageEmbed } = require('discord.js');
 const Discord = require('discord.js');
 const client = new Discord.Client();
 client.on('message', msg=>{
-    if (msg.content === `-roleadd twitch`) {
+    if (msg.content === `-rolerm twitch`) {
         // Set the member's roles to a single role
        var role= msg.member.guild.roles.cache.find(role => role.name === "Twitch Pings");
        msg.member.roles.remove(role);
@@ -12,7 +12,7 @@ client.on('message', msg=>{
   client.off
 })
 client.on('message', msg=>{
-    if (msg.content === `-roleadd youtube`) {
+    if (msg.content === `-rolermd youtube`) {
         // Set the member's roles to a single role
        var role= msg.member.guild.roles.cache.find(role => role.name === "Youtube Pings");
        msg.member.roles.remove(role);
@@ -22,7 +22,7 @@ client.on('message', msg=>{
   client.off
 })
 client.on('message', msg=>{
-    if (msg.content === `-roleadd tiktok`) {
+    if (msg.content === `-rolerm tiktok`) {
         // Set the member's roles to a single role
        var role= msg.member.guild.roles.cache.find(role => role.name === "TikTok Pings");
        msg.member.roles.remove(role);
