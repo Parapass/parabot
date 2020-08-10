886ce72e0f14cfe08eea730fa65c1eb5b2b72811
Parapass <59424861+Parapass@users.noreply.github.com>
2020-08-10 09:33:20 -0400

    idk

--- roles.js
+++ roles.js
@@ -1,1 +1,31 @@
 
+client.on('message', msg=>{
+    if (msg.content === `-roleadd twitch`) {
+        // Set the member's roles to a single role
+       var role= msg.member.guild.roles.cache.find(role => role.name === "Twitch Pings");
+       msg.member.roles.add(role);
+      console.log('Added twitch role successfully.')
+      msg.channel.send('Added Twitch Pings role successfully.')
+    }
+  client.off
+})
+client.on('message', msg=>{
+    if (msg.content === `-roleadd youtube`) {
+        // Set the member's roles to a single role
+       var role= msg.member.guild.roles.cache.find(role => role.name === "Youtube Pings");
+       msg.member.roles.add(role);
+      console.log('Added youtube role successfully.')
+      msg.channel.send('Added Youtube Pings role successfully.')
+    }
+  client.off
+})
+client.on('message', msg=>{
+    if (msg.content === `-roleadd tiktok`) {
+        // Set the member's roles to a single role
+       var role= msg.member.guild.roles.cache.find(role => role.name === "TikTok Pings");
+       msg.member.roles.add(role);
+      console.log('Added TikTok role successfully.')
+      msg.channel.send('Added TikTok Pings role successfully.')
+    }
+  client.off
+})
